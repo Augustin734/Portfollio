7ec191d495d5f06caa1837f7f67ef1361be4404b
Augustin <augustin.viemont1@outlook.fr>
2026-01-19 12:14:46 +0100

    Fix GitHub Pages base path

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,3 @@
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
-
-// // https://vite.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-//   base: './',
-//   server: {
-//     host: true,
-//     port: 5173,
-//   },
-//   build: {
-//     outDir: 'dist'
-//   }
-// })
-
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
@@ -21,7 +5,6 @@ export default defineConfig({
   plugins: [react()],
   base: '/Portfollio/', // IMPORTANT pour GitHub Pages
   build: {
-    outDir: 'dist',      // dossier de build
+    outDir: 'dist',
   },
 })
-
