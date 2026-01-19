ab74388fc8a246934c0257475dde6eb290d1a6eb
Augustin <augustin.viemont1@outlook.fr>
2026-01-19 12:04:59 +0100

    fixed bug

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,27 @@
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+//   base: './',
+//   server: {
+//     host: true,
+//     port: 5173,
+//   },
+//   build: {
+//     outDir: 'dist'
+//   }
+// })
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Portfollio/',
-  server: {
-    host: true,
-    port: 5173,
-  },
+  base: '/Portfollio/', // IMPORTANT pour GitHub Pages
   build: {
-    outDir: 'dist'
-  }
+    outDir: 'dist',      // dossier de build
+  },
 })
+
